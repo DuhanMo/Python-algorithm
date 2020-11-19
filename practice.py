a0cf75a403aad5dabf357033842912b16bde8425
DuhanMo <devduhanmo@gmail.com>
2020-11-20 01:57:27 +0900

    20201120 파이썬 백준문제풀이

---
+++ practice.py
@@ -0,0 +1,39 @@
+print(5)
+
+print(-10)
+print(3.14)
+print(3.14)
+
+
+print('풍선')
+print(True)
+print(not True)
+
+station = '사당'
+print(station + '행 열차가 들어오고 있습니다.')
+
+station = '인천공항'
+station = '신도림'
+
+print(abs(-5))
+# 4^2
+print(pow(4,2))
+
+#12
+print(max(5,12))
+print(min(5,12))
+
+print(round(3.14))
+print(round(4.99))
+from math import *
+print(floor(4.99))  # 내림
+print(ceil(4.99))  # 올림
+print(sqrt(16))  # 제곱근
+from random import *
+
+print(random())
+print(random()*10)
+
+
+
+
