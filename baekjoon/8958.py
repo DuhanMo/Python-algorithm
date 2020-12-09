d114571426d945699c6c2de4421fc7ce75add4c1
DuhanMo <devduhanmo@gmail.com>
2020-12-09 22:05:17 +0900

    20201209 백준

--- baekjoon/8958.py
+++ baekjoon/8958.py
@@ -12,3 +12,19 @@ for i in range(n):
             score = 0
     print(sum_score)
 
+
+def qs(array):
+    for i in range(len(array)):
+        minimum = i
+        for j in range(i+1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+
+        temp = array[i]
+        array[i] = array[minimum]
+        array[minimum] = temp
+    return array
+
+
+
+
