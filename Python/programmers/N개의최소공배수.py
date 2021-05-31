3100aa01fefab1bb5c2da07dba3754ab026f698a
DuhanMo <devduhanmo@gmail.com>
2021-05-31 17:38:35 +0900

    N개의 최소공배수 프로그래머스

---
+++ Python/programmers/N개의최소공배수.py
@@ -0,0 +1,31 @@
+def gcd(a, b):
+    if a%b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+def solution(arr):
+    answer = 0
+    l = lcm(arr[0], arr[1])
+    for i in range(2, len(arr)):
+        l = lcm(l, arr[i])
+
+    return l
+
+import math
+
+def solution2(arr):
+    answer = 0
+    l = math.lcm(arr[0], arr[1])
+    for i in range(2, len(arr)):
+        l = math.lcm(l, arr[i])
+
+    return l
+
+
+arr = [2,6,8,14]
+print(solution2(arr))
