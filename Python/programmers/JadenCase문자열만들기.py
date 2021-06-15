70c3290462e76766b7640a7ba515767a7721818a
DuhanMo <devduhanmo@gmail.com>
2021-06-15 20:18:58 +0900

    JadenCase문자열만들기.py

--- Python/programmers/JadenCase문자열만들기.py
+++ Python/programmers/JadenCase문자열만들기.py
@@ -1,10 +1,19 @@
 def solution(s):
-    answer = ' '
+    last_space = False
+    if s[-1] == ' ':
+        last_space = True
+    print(last_space)
+    answer = ''
     s = s.split(' ')
     for x in s:
         answer += x.capitalize() + ' '
 
-    return answer.strip()
+    answer = answer.strip()
+    if last_space:
+        return answer + ' '
+    else:
+        return answer
 
-s = "for the last week"
+
+s = "for the last week "
 print(solution(s))
